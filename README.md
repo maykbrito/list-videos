f7139d5d8ef8612c8703c85897846f6de61bf691
Mayk Brito <maykbrito@gmail.com>
2020-04-26 11:08:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Today
+
+* [x] Ajuste de arte do Canal
+* [x] Tela de espera da live (ajustar depois)
+
+
+## Listagem de vídeos do canal
+
+### API
+* [x] Estrutura de pastas
+* [x] Instalação dos módulos
+* [x] Configuração das chaves do Google
+* [x] Buscar conteúdo na API do Google
+* [x] NotFound Page / Error Handler
+* [x] Apresentação/Listagem do conteúdo
+* [x] Cache de busca
+* [x] Se tiver mais páginas?
+* [x] Organização dos dados
+* [] Deploy
+
+### Frontend
+* [] Estrutura de Pastas
+* [] Arquivos básicos (html, css, js)
+* [] Sections: Header /  Search / Videos
+* [] Estilização
+* [] Puxar dados da API
+* [] Organização e apresentação dos dados no HTML
+* [] Filtro de pesquisa / Keyup Event
+* [] Filtrar dados
---
+++ server/src/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const { GOOGLE_KEY } = require('../.env')
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+
+let cache, 
+    url = `https://www.googleapis.com/youtube/v3/playlistItems?maxResults=50&part=snippet&playlistId=PLeLKux5eT3kaXJ4p_P96I9lr7qEpnbWTY&key=${GOOGLE_KEY}`
+
+const getVideos = pageToken => 
+    axios.get(url + (pageToken ? `&pageToken=${pageToken}` : '')).then( res => res.data)
+
+const organizeVideos = items => items.map( ({snippet}) => {
+    return {
+        publishedAt: snippet.publishedAt,
+        title: snippet.title,
+        description: snippet.description,
+        thumbnails: snippet.thumbnails
+    }
+})
+
+app.get('/videos', async (req, res) => {
+    if (cache) return res.json(cache)
+    
+    try {
+        let data = await getVideos(),
+            videos = organizeVideos(data.items)
+            
+        while(data.nextPageToken) {
+            data = await getVideos(data.nextPageToken)
+            videos = videos.concat(organizeVideos(data.items))
+        }
+
+        cache = videos
+        return res.json(videos)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+const notFound = (req, res, next) => {
+    res.status(404)
+    const error = new Error('NOT_FOUND')
+    next(error)
+}
+
+const errorHandler = (error, req, res, next) => {
+    res.status(res.statusCode || 500)
+    res.json({ message: error.message })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(3333, () => console.log('Server is running'));
